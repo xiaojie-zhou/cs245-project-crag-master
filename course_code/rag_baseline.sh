280d69b3e62550b81dbc24c57a0591cdb8f20a23
Johanna Bai <johannabai102@ucla.edu>
2024-12-04 07:24:05 +0000

    added rag_baseline.sh

---
+++ course_code/rag_baseline.sh
@@ -0,0 +1,5 @@
+python generate.py \
+    --dataset_path "../../../../data/crag_task_1_dev_v4_release.jsonl.bz2" \
+     --split 1 \
+    --model_name "rag_baseline" \
+    --llm_name "meta-llama/Llama-3.2-3B-Instruct"
